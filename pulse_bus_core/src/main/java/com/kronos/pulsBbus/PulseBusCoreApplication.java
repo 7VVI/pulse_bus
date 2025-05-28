0b3388d42ce0a1cd928dfe75a92a97bf71b020b3
zhangyuhong <mc1753343931@gmail.com>
2025-05-28 14:29:38 +0800

    feat: 初始化项目

---
+++ pulse_bus_core/src/main/java/com/kronos/pulsBbus/PulseBusCoreApplication.java
@@ -0,0 +1,13 @@
+package com.kronos.pulsBbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseBusCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulseBusCoreApplication.class, args);
+    }
+
+}
---
+++ pulse_bus_core/src/test/java/com/kronos/pulsBbus/PulseBusCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kronos.pulsBbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseBusCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
